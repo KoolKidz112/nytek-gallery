b06a880e280c85c50cdca4dea6b6cec1be9f95ea
hoylecake <thekidcorner@gmail.com>
2021-12-15 20:52:34 -0500

    We ARe Pensiing With Nycrite. he Does noT Know Js. Silley man using require()

--- src/crypto.js
+++ src/crypto.js
@@ -1,31 +1,30 @@
-function generateNFT() {
-  var randomstring = require("randomstring");
-  // Make potential NFT name
-  const name = [
-    "Hoylepunk #1",
-    "Hoylepunk #2",
-    "Hoylepunk #3",
-    "Hoylepunk #4",
-    "baZOOKa #1",
-    "baZOOKa #2",
-    "baZOOKa #3",
-    "baZOOKa #4",
-    "Russian Spy"
-  ];
-  // Name-Generator 1375413573871984 thousan
-  var nftname = name[Math.floor(Math.random() * name.length)];
-  // Every NFT need a hash bro!
-  var hash = randomstring.generate(30);
-  // Set owner to none
-  var price = Math.floor(Math.random() * 11);
+const randString = () =>
+  Math.random().toString(36).substring(2, 15) +
+  Math.random().toString(36).substring(2, 15);
+const name = [
+  "Hoylepunk #1",
+  "Hoylepunk #2",
+  "Hoylepunk #3",
+  "Hoylepunk #4",
+  "baZOOKa #1",
+  "baZOOKa #2",
+  "baZOOKa #3",
+  "baZOOKa #4",
+  "Russian Spy",
+];
 
-  document.getElementById("name").innerHTML = nftname;
-  document.getElementById("hash").innerHTML = hash;
-  document.getElementById("price").innerHTML = price;
+class NFT {
+  constructor() {
+    this.name = name[Math.floor(Math.random() * name.length)];
+    this.hash = randString();
+    this.price = Math.floor(Math.random() * 11);
+  }
+
+  render() {
+    document.getElementById("name").innerHTML = nftname;
+    document.getElementById("hash").innerHTML = hash;
+    document.getElementById("price").innerHTML = price;
+  }
 }
-// YOU ARE FUCKING KIDDING ME
-// I FORGOT TO SAVE
-// I FIXED SO MUCH SHTI
-// GOD
-// FUCKING
-// DAMMIT
+
+const generateNFT = () => new NFT().render();
